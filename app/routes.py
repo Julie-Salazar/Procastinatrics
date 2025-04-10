aacfba0cd63091acd4bc450c4290995879ce9564
ozzy1323232 <168622926+ozzy1323232@users.noreply.github.com>
2025-04-10 14:08:06 +0800

    front end linking to backend

--- app/routes.py
+++ app/routes.py
@@ -4,6 +4,7 @@ from app import app
 from .database import *
 
 from .models import *
+from .forms import *
 from flask_login import LoginManager, UserMixin, login_user, logout_user, login_required, current_user
 import os
 from datetime import datetime, date
@@ -16,20 +17,29 @@ import pandas as pd
 def login():
 
     # for front end change (uid = 1) to the form input box data : eg user types in password '1234' (uid = form.password)
-    user = GetUser(user_name='zac123')
-    
+    if current_user.is_authenticated:
+        
+        return flask.redirect('home')
 
-    if user is None:
-        # username is not found
-        return "username not found"
-    
-    if user.is_password_correct("password1234") == False:
-        return "Password is incorrect"
-    
-    login_user(user)
+
+    form = LoginForm()
+
+    if form.validate_on_submit():
+        user = GetUser(email=form.email.data)
+
+        if user is None:
+            # username is not found
+            return flask.redirect('login')
+
+        
+        if user.is_password_correct(form.password.data) == False:
+            return flask.redirect('login')
+        
+        login_user(user)
+        return flask.redirect('home')
 
     # below for the front end change to (return login.html)
-    return "logged in as " + user.first_name + " " + user.last_name + " with password " + user.passwordHash
+    return flask.render_template("login.html", form=form)
 
 
 @app.route('/home')
