6ec50df15912e6a065e759d6857f4f35203b3df5
“seventhirty233” <seventhirty233@outlook.com>
2025-04-18 22:34:36 +0800

    Added global and local css files for the login page. Modified forms.py: email = StringField -> email = EmailField for the correct type of the input field

--- app/forms.py
+++ app/forms.py
@@ -1,11 +1,11 @@
 from flask_wtf import FlaskForm
 from flask_wtf.file import FileAllowed, FileRequired
-from wtforms import StringField, PasswordField, BooleanField, SubmitField, SelectField, SelectMultipleField, HiddenField, FileField, DateField, widgets, TextAreaField
+from wtforms import EmailField, StringField, PasswordField, BooleanField, SubmitField, SelectField, SelectMultipleField, HiddenField, FileField, DateField, widgets, TextAreaField
 from wtforms.validators import DataRequired, ValidationError, Length
 
 # this is the form schema for loggin in 
 class LoginForm(FlaskForm):
-    email = StringField('email:', validators=[DataRequired()])
+    email = EmailField('email:', validators=[DataRequired()])
     password = PasswordField('Password:', validators=[DataRequired()])
     submit = SubmitField('Log In')
     remember_me = BooleanField('Remember Me')
