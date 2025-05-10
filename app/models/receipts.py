6d35eac158c9bb9b6af483a1c2d9c2ff2316b908
Daimy0u <yusukematsuki123@gmail.com>
2025-05-10 17:51:40 +0800

    add: share routing and changed database models to avoid redundancy.

--- app/models/receipts.py
+++ app/models/receipts.py
@@ -1,6 +1,12 @@
 from app import db
 from . import User,BaseModel
 
+class Status:
+    IGNORED = 'Ignored'
+    PENDING = 'Pending'
+    DECLINED = 'Declined'
+    BLOCKED = 'Blocked'
+    
 class Receipts(BaseModel):
     """
     Stores data instead of accessing other tables.
@@ -12,19 +18,11 @@ class Receipts(BaseModel):
     hours_gaming = db.Column("hours_gaming",db.Integer)
     hours_productive = db.Column("hours_productive",db.Integer)
     
-class ReceiptsShared(BaseModel):
-    """
-    Keeps track of receipt sharing/access.
-    """
-    share_id = db.Column(db.Integer, primary_key=True)
-    author_id = db.Column("author_id",db.Integer,db.ForeignKey("user.uid"))
-    receiver_id = db.Column("receiver_id",db.Integer,db.ForeignKey("user.uid"))
-    receipt_id = db.Column("receipt_id",db.Integer,db.ForeignKey("receipts.receipt_id"))
 
-class ReceiptShareRequest(BaseModel):
+class ReceiptsShareRequest(BaseModel):
     request_id = db.Column("request_id", db.Integer, primary_key=True)
     sender_id = db.Column("sender_id", db.Integer, db.ForeignKey("user.uid"))
-    receiver_id = db.Column("receiver_id", db.Integer, db.ForeignKey("user.uid"))
+    receiver_id = db.Column("recipient_id", db.Integer, db.ForeignKey("user.uid"))
     shared_receipt_id = db.Column("shared_receipt_id", db.Integer, db.ForeignKey("receipts.receipt_id"))
     status = db.Column("status",db.String(20))
     time = db.Column("time",db.Integer)
--- app/routes/share_route.py
+++ app/routes/share_route.py
@@ -17,17 +17,58 @@
 #     ]
 #     return render_template('share.html', friends=friends_list)
 
+from sqlalchemy import select
+from flask import Blueprint, render_template, request, flash
+from flask_login import login_required, current_user
 
-from flask import Blueprint, render_template, request
-from flask_login import login_required
+from app.models.receipts import ReceiptsShareRequest, Status
+from app.models.user import User
+from app import db
 
 share = Blueprint('share', __name__)
 
 @share.route('/share', methods=['GET', 'POST'])
 @login_required
 def share_page():
-    if request.method is 'POST':
-        #TODO
-        pass
+    if request.method == 'POST':
+        sender_id = current_user.id
+        recipient_id = request.form.get('recipient_id')
+        receipt_id = request.form.get('receipt_id')
+        
+        # Check if user exists
+        recipient_user = User.query.filter_by(uid=current_user.id).first()
+        if not recipient_user: 
+            flash("User not found!", 'error')
+            return render_template('share.html'), 403
+        
+        # Check if request exists
+        query = ReceiptsShareRequest.query.filter_by(sender_id=sender_id,
+                                                     recipient_id=recipient_id,
+                                                     shared_receipt_id=receipt_id
+                                                    ).first()
+        if query:
+            if query.status != Status.IGNORED:
+                return '', 403
+            elif query.status == Status.IGNORED:
+                # Update request (resend)
+                query.status = Status.PENDING
+                query.time = db.func.current_timestamp()
+                db.session.add(query)
+                db.session.commit()
+                flash(f"Resent request to {recipient_user.first_name}!")
+                return str(query), 200
 
-    return render_template('share.html')  # update this if your template name is different
+        # Create a new share request
+        share_request = ReceiptsShareRequest(
+            sender_id=current_user.id,
+            recipient_id=recipient_id,
+            shared_receipt_id=receipt_id,
+            status = Status.PENDING,
+            time = db.func.current_timestamp()
+        )
+        
+        db.session.add(share_request)
+        db.session.commit()
+        flash(f"Sent request to {recipient_user.first_name}!", 'success')
+        return str(query),200
+    return render_template('share.html')  
